57638dc4d258ccca75c3a2fb0c7b998c947c7624
Priyanshu132 <priyanshugupta132@gmail.com>
2021-11-09 23:57:39 +0530

    Second

---
+++ app/src/main/java/com/hapini/pvt/safety/Recorded_images.java
@@ -0,0 +1,13 @@
+package com.hapini.pvt.safety;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recorded_images extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recorded_images);
+    }
+}
